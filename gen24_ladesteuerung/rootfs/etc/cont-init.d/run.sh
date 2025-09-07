ae051d9a88b78def4ca7338a2ac5877d55e05dce
roethigj <roethig_j@web.de>
2025-09-07 17:49:42 +0200

    Update run.sh

--- gen24_ladesteuerung/rootfs/etc/cont-init.d/run.sh
+++ gen24_ladesteuerung/rootfs/etc/cont-init.d/run.sh
@@ -128,25 +128,25 @@ if [ -f "$Gen24_Path/cron_draft" ]; then
     else
         cp $Gen24_Path/cron_draft $Gen24_Path/cron_file
     fi
-    if [ ! "$(bashio::config 'akkudoktor_weather')" ]; then
+    if [ ! "$(bashio::config 'akkudoktor_weather')"='true' ]; then
         sed -i "s*#akku**g" "$Gen24_Path/cron_file"
     fi
-    if [ ! $(bashio::config 'forecast_solar') ]; then
+    if [ ! "$(bashio::config 'forecast_solar')"='true' ]; then
         sed -i "s*#forecast**g" "$Gen24_Path/cron_file"
     fi
-    if [ ! $(bashio::config 'solcast') ]; then
+    if [ ! "$(bashio::config 'solcast')"='true' ]; then
         sed -i "s*#solcast**g" "$Gen24_Path/cron_file"
     fi
-    if [ ! $(bashio::config 'solcast_homeassistant') ]; then
+    if [ ! "$(bashio::config 'solcast_homeassistant')"='true' ]; then
         sed -i "s*#sol_ha**g" "$Gen24_Path/cron_file"
     fi
-    if [ ! $(bashio::config 'openmeteo')  ]; then
+    if [ ! "$(bashio::config 'openmeteo')"='true'  ]; then
         sed -i "s*#open**g" "$Gen24_Path/cron_file"
     fi
-    if [ ! $(bashio::config 'solarprognose') ]; then
+    if [ ! "$(bashio::config 'solarprognose')"='true' ]; then
         sed -i "s*#sol_prog**g" "$Gen24_Path/cron_file"
     fi
-    if [ ! $(bashio::config 'dynamic_price_check') ]; then
+    if [ ! "$(bashio::config 'dynamic_price_check')"='true' ]; then
         sed -i "s*#dyn_price**g" "$Gen24_Path/cron_file"
     fi
     sed -i "s#c_int#$c_int#g" "$Gen24_Path/cron_file"
@@ -154,13 +154,11 @@ if [ -f "$Gen24_Path/cron_draft" ]; then
 
 fi
 crontab $Gen24_Path/cron_file
-echo $kennwort
-if [ ! $kennwort="password" ]; then
+
+if [ ! "$kennwort" = "password" ]; then
     echo "ich starte"
     $Gen24_Path/start_PythonScript.sh http_SymoGen24Controller2.py
 fi
-
-echo "crond"
 crond
 
 exit 0
