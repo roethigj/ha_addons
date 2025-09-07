5a69191271fef1583bbe362c0e4d01566bf1ebd8
roethigj <roethig_j@web.de>
2025-09-07 19:41:40 +0200

    Update run.sh

--- gen24_ladesteuerung/rootfs/etc/cont-init.d/run.sh
+++ gen24_ladesteuerung/rootfs/etc/cont-init.d/run.sh
@@ -153,14 +153,9 @@ crontab $Gen24_Path/cron_file
 #if [ ! "$kennwort" = "password" ]; then
 #    $Gen24_Path/start_PythonScript.sh http_SymoGen24Controller2.py
 #fi
-
-response="$($Gen24_Path/start_PythonScript.sh http_SymoGen24Controller2.py 2>&1)"
-# Did NOT set `set -e`, hence script continues even if there's an error
-if [[ $? -eq 0 ]]; then
-    echo "Success: $response"
-else
-    echo "Error: $response"
-fi
+set -e 
+response="$($Gen24_Path/start_PythonScript.sh http_SymoGen24Controller2.py 2>&1 || true)"
+echo "Result - ${response}"
 
 crond
 
