0e1fbc137043c470064090deda62fac4c27c24bd
roethigj <roethig_j@web.de>
2025-11-14 18:15:53 +0100

    e2m init

--- eebus2mqtt/run.sh
+++ eebus2mqtt/run.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/with-contenv bashio
-CONFIG_PATH="/data/options.json"
-DATA_PATH="/data"
-
-remote_SKI=$(bashio::config 'SKI')
-mqttBroker=$(bashio::config 'MQTT.host')
-mqttUser=$(bashio::config 'MQTT.user')
-mqttPassword=$(bashio::config 'MQTT.password')
-mqttPort=$(bashio::config 'MQTT.port')
-inverter=$(bashio::config 'inverter_max')
-echo “Testing this thing!”
-# CONFIG mit Benutzereingaben erzeugen
-if [ -f "$Data_Path/config.json" ]; then
-  sed -i \
-    -e "s/\"remoteSki\": *\"[^\"]*\"/\"remoteSki\": \"$remote_SKI\"/" \
-    -e "s/\"inverter_max\": *\"[^\"]*\"/\"remoteSki\": \"$inverter\"/" \
-    -e "s/\"mqttBroker\": *\"[^\"]*\"/\"mqttBroker\": \"$mqttBroker\"/" \
-    -e "s/\"mqttPort\": *[0-9]*/\"mqttPort\": $mqttPort/" \
-    -e "s/\"mqttUsername\": *\"[^\"]*\"/\"mqttUsername\": \"$mqttUser\"/" \
-    -e "s/\"mqttPassword\": *\"[^\"]*\"/\"mqttPassword\": \"$mqttPassword\"/" \
-    "$Data_Path/config.json"
-fi
 
 # Start Go application
 /usr/local/bin/hems-device
