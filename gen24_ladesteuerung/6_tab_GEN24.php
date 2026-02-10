8e62344316b000b1149a8c7e5a30183201f7b5ce
roethigj <roethig_j@web.de>
2026-02-10 19:39:03 +0100

    test

--- gen24_ladesteuerung/6_tab_GEN24.php
+++ gen24_ladesteuerung/6_tab_GEN24.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="de">
 <body>
-<iframe src="GEN24_proxy.php"
+<iframe src="/device/"
         style="width:100%;height:100%;border:0;"></iframe>
 </body>
 </html>
--- gen24_ladesteuerung/rootfs/etc/cont-init.d/run.sh
+++ gen24_ladesteuerung/rootfs/etc/cont-init.d/run.sh
@@ -168,6 +168,30 @@ crontab $Gen24_Path/cron_file
 
 $Gen24_Path/start_PythonScript.sh EnergyController.py 2>&1
 
+# nginx
+#!/bin/sh
+set -e
+
+CONFIG="$Gen24_Path/CONFIG/default.ini"
+PRIV="$Gen24_Path/CONFIG/default_priv.ini"
+
+FILE="$CONFIG"
+[ -f "$PRIV" ] && FILE="$PRIV"
+
+TARGET_HOST=$(grep -E '^hostNameOrIp=' "$FILE" | cut -d= -f2 | tr -d ' \r\n')
+
+if [ -z "$TARGET_HOST" ]; then
+  echo "hostNameOrIp nicht gefunden"
+  exit 1
+fi
+
+sed "s|__TARGET_HOST__|$TARGET_HOST|g" \
+    /etc/nginx/nginx.conf.template \
+    > /etc/nginx/nginx.conf
+
+exec nginx -g "daemon off;"
+
+
 crond
 
 exit 0
