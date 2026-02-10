49be21e1b473b8608522c452311f7714acee3d63
roethigj <roethig_j@web.de>
2026-02-10 21:02:21 +0100

    test

--- gen24_ladesteuerung/run_nginx.sh
+++ gen24_ladesteuerung/run_nginx.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/with-contenv bashio
 set -e
 Gen24_Path="/opt/Gen24_Ladesteuerung"
+CONF_FILE="/etc/nginx/http.d/gen24.conf"
 
 CONFIG="$Gen24_Path/CONFIG/default.ini"
 PRIV="$Gen24_Path/CONFIG/default_priv.ini"
@@ -12,13 +13,11 @@ TARGET_HOST=$(grep -E '^hostNameOrIp=' "$FILE" | cut -d= -f2 | tr -d ' \r\n')
 
 if [ -z "$TARGET_HOST" ]; then
   echo "hostNameOrIp nicht gefunden"
-  exit 0
+  exit 1
 fi
 
-sed "s|__TARGET_HOST__|$TARGET_HOST|g" \
-    /etc/nginx/nginx.conf.template \
-    > /etc/nginx/nginx.conf
+sed -i "s|__TARGET_HOST__|$TARGET_HOST|g" "$CONF_FILE"
 
-exec nginx -g "daemon off;"
+exec nginx
 
 exit 0
