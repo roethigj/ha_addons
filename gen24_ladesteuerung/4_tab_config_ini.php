543c63065ab050960d52e02a6f78bac736ca08b4
roethigj <roethig_j@web.de>
2026-02-11 23:27:25 +0100

    test

--- gen24_ladesteuerung/4_tab_config_ini.php
+++ gen24_ladesteuerung/4_tab_config_ini.php
@@ -748,10 +748,7 @@ if (!copy($ini_file, $backup_file)) {
 
 // header('location: '.$_SERVER["PHP_SELF"].'?nachricht='.$nachricht);
 // NEU (funktioniert auch nach HTML-Ausgabe)
-//$redirect_url = $_SERVER["PHP_SELF"] . "?nachricht=" . urlencode('gesichert') . "&tab=" . $activeTab;
-$redirect_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') 
-    . '://' . $_SERVER['HTTP_HOST'] 
-    . $_SERVER['REQUEST_URI'] . "?nachricht=" . urlencode('gesichert') . "&tab=" . $activeTab;
+$redirect_url = $_SERVER["PHP_SELF"] . "?nachricht=" . urlencode('gesichert') . "&tab=" . $activeTab;
 //echo "<script type='text/javascript'>window.location.href='$redirect_url';</script>";
 echo '<meta http-equiv="refresh" content="0;url=' . $redirect_url . '">';
 
