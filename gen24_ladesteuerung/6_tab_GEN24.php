510c3dcdb432f35aed1ba060d53af978db90e05f
roethigj <roethig_j@web.de>
2026-02-17 12:08:58 +0100

    Refactor hostNameOrIp retrieval and update output

--- gen24_ladesteuerung/6_tab_GEN24.php
+++ gen24_ladesteuerung/6_tab_GEN24.php
@@ -1,38 +1,50 @@
 <?php
 require_once "config_parser.php";
 
-// Config-Datei wählen
 $file = $PythonDIR.'/CONFIG/default.ini';
-if(file_exists($PythonDIR.'/CONFIG/default_priv.ini')){
+if (file_exists($PythonDIR.'/CONFIG/default_priv.ini')) {
     $file = $PythonDIR.'/CONFIG/default_priv.ini';
 }
 
-// Nachricht ausgeben, falls vorhanden
 $nachricht = $_GET["nachricht"] ?? '';
-if ($nachricht != '') echo $nachricht . "<br><br>";
+if ($nachricht !== '') {
+    echo htmlspecialchars($nachricht) . "<br><br>";
+}
 
-// hostNameOrIp auslesen
 $host = '';
-$fh = fopen($file, "r") or die("Kann Datei ".$file." nicht öffnen!");
-while(!feof($fh)) {
-    $line = fgets($fh);
-    if (strpos($line, 'hostNameOrIp') !== false && strpos($line, '=') !== false) {
-        [, $value] = explode('=', $line, 2);
+
+$myfile = fopen($file, "r") or die("Kann Datei $file nicht öffnen!");
+while (!feof($myfile)) {
+    $zeile = fgets($myfile);
+    if (strpos($zeile, 'hostNameOrIp') !== false && strpos($zeile, '=') !== false) {
+        [$key, $value] = explode("=", $zeile, 2);
         $host = trim($value);
         break;
     }
 }
-fclose($fh);
+fclose($myfile);
 
-if ($host === '') die("hostNameOrIp nicht gefunden");
+if ($host === '') {
+    die("hostNameOrIp nicht gefunden");
+}
+?>
 
-// Ziel-URL: intern HTTP, extern als HTTPS
-//$target_url = "http://$host/";
-
-// Browser Redirect
-// Hier trick: wir nutzen Home Assistant Ingress HTTPS URL
-// In HA-Ingress-iframe ist der HTTPS-Pfad immer korrekt
-//header("Content-Security-Policy: upgrade-insecure-requests"); // optional, erzwingt HTTPS
-//header("Location: http://$host");
-echo "<iframe src='proxy.php' style='width:100%;height:100%;border:0'></iframe>";
-exit;
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Ingress Anzeige</title>
+    <style>
+        html, body, iframe {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            border: 0;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://<?= htmlspecialchars($host) ?>"></iframe>
+</body>
+</html>
